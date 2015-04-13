9c078ad91afadbcd4b1b83065f8deeb5bed5d4a6
Max Mulatz <klappradla@gmail.com>
2015-04-13 22:11:24 +0200

    change gruntfile to keep svg ids

--- Gruntfile.js
+++ Gruntfile.js
@@ -219,6 +219,15 @@ module.exports = function (grunt) {
     },
 
     svgmin: {
+      options: {
+        full: true,
+        plugins: [
+          {cleanupIDs: false},                  // don't remove  ids
+          {removeViewBox: false},               // don't remove the viewbox atribute from the SVG
+          {removeUselessStrokeAndFill: false},  // don't remove Useless Strokes and Fills
+          {removeEmptyAttrs: false}             // don't remove Empty Attributes from the SVG
+        ]
+      },
       dist: {
         files: [{
           expand: true,
